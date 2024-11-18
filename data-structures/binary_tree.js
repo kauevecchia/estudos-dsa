45d5b5fef06f5e5b43538ccdc592967765b7a12c
Kauê <kauevecchia@hotmail.com>
2024-11-18 00:37:25 -0300

    binary tree traversals

--- data-structures/binary_tree.js
+++ data-structures/binary_tree.js
@@ -51,16 +51,63 @@ class binaryTree {
             return this.search_recursive(node.right, data)
         }
     }
+
+    preorder_traversal() {
+        let result = []
+        this.preorder_recursive(this.root, result)
+        return result
+    }
+
+    preorder_recursive(node, result) {
+        if (node) {
+            result.push(node.data)
+            this.preorder_recursive(node.left, result)
+            this.preorder_recursive(node.right, result)
+        }
+    }
+
+    inorder_traversal() {
+        let result = []
+        this.inorder_recursive(this.root, result)
+        return result
+    }
+
+    inorder_recursive(node, result) {
+        if (node) {
+            this.inorder_recursive(node.left, result)
+            result.push(node.data)
+            this.inorder_recursive(node.right, result)
+        }
+    }
+
+    postorder_traversal() {
+        let result = []
+        this.postorder_recursive(this.root, result)
+        return result
+    }
+
+    postorder_recursive(node, result) {
+        if (node) {
+            this.postorder_recursive(node.left, result)
+            this.postorder_recursive(node.right, result)
+            result.push(node.data)
+        }
+    }
 }
 
 const tree = new binaryTree()
 tree.insert(5)
 tree.insert(3)
-tree.insert(8)
 tree.insert(1)
-tree.insert(4)
+tree.insert(10)
+tree.insert(15)
 tree.insert(7)
-tree.insert(9)
 
-console.log(`Search 4: ${tree.search(4)}.`)
-console.log(`Search 6: ${tree.search(6)}.`)
+// preorder: [5, 3, 1, 10, 7, 15]
+// inorder: [1, 3, 5, 7, 10, 15]
+// postorder: [1, 3, 7, 15, 10, 5]
+
+
+console.log(`preorder_traversal: ${tree.preorder_traversal()}.`)
+console.log(`inorder_traversal: ${tree.inorder_traversal()}.`)
+console.log(`postorder_traversal: ${tree.postorder_traversal()}.`)
