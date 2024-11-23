cbbae68d8618ff4069f09852d124e99ffe706bd3
Kauê <kauevecchia@hotmail.com>
2024-11-23 18:10:35 -0300

    linked list stack implementation

--- data-structures/stack.js
+++ data-structures/stack.js
@@ -1,29 +1,52 @@
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
 class Stack {
     constructor() {
-        this.items = []
+        this.top = null
+        this._size = 0
     }
 
     push(item) {
+        new_node = new Node(item)
+        new_node.next = this.top
+        this.top = new_node
+        this._size += 1
         this.items.push(item)
     }
 
     pop() {
-        if(!this.items.length) {
-            throw new Error("Empty list")
+        if(this.top == null) {
+            throw new Error("Empty stack")
         }
 
-        return this.items.shift()
+        let popped_node = this.top
+        this.top = popped_node.next
+        this.size -= 1
+        return popped_node.value
     }
 
     peek() {
-        if(!this.items.length) {
-            throw new Error("Empty list")
+        if(this.top == null) {
+            throw new Error("Empty stack")
         }
 
-        return this.items[-1]
+        return this.top.value
     }
 
     size() {
-        return this.items.length
+        return this._size
     }
 }
+
+let stack = new Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+console.log(stack.pop()) // Output: 3
+console.log(stack.peek()) // Output: 2
+console.log(stack.size()) // Output: 2
